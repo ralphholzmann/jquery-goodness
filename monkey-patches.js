3f31f2c42c452d8daca8807ca354abd0e6560287
Ralph Holzmann <ralphholzmann@gmail.com>
2010-01-28 16:41:19 -0600

    Added license.

--- monkey-patches.js
+++ monkey-patches.js
@@ -1,3 +1,6 @@
+// Assorted jQuery feature enhancements.
+// Ralph Holzmann. MIT License.
+
 (function($){
 
     /***
@@ -6,12 +9,12 @@
      * next() and next(<string selector>) still in tact
     */
     var _next = $.fn.next;
-    
+
     $.fn.next = function(selector) {
         if ( typeof (selector) === "number" && selector > 1) {
-            return $(jQuery.nth( this[0], ( selector + 1 ), "nextSibling" ));                        
+            return $(jQuery.nth( this[0], ( selector + 1 ), "nextSibling" ));
         }
-        
+
         return _next.apply(this, arguments);
     }
 
@@ -22,10 +25,10 @@
      * prev() and prev(<string selector>) still in tact
     */
     var _prev = $.fn.prev;
-    
+
     $.fn.prev = function(selector) {
         if ( typeof (selector) === "number" && selector > 1) {
-            return $(jQuery.nth( this[0], ( selector + 1 ), "previousSibling" ));                        
+            return $(jQuery.nth( this[0], ( selector + 1 ), "previousSibling" ));
         }
 
         return _prev.apply(this, arguments);
@@ -38,7 +41,7 @@
      * parent() and parent(<string selector>) still in tact
     */
     var _parent = $.fn.parent;
-    
+
     $.fn.parent = function(selector) {
         if ( typeof (selector) === "number" && selector > 1) {
             var parent = this[0].parentNode;
@@ -47,21 +50,21 @@
             }
             return parent && parent.nodeType !== 11 ? $(parent) : null;
         }
-        
+
         return _parent.apply(this, arguments);
     }
 
 })(jQuery);
 
 (function($){
-        
+
     /***
      * clone() accepts numerical argument
      * returns an array containing n clones
      * clone() and clone(withDataAndEvents) still in tact
     */
     var _clone = $.fn.clone;
-    
+
     $.fn.clone = function(n, withDataAndEvents) {
         if ( typeof (n) === "number" && n > 1) {
             var stack = [];
@@ -72,38 +75,38 @@
         }
 
         return _clone.call(this, withDataAndEvents);
-    }       
+    }
 
 })(jQuery);
 
 (function($){
-        
+
     /***
      * fadeIn() and fadeOut() now removes filter attribute in IE to resolve
      * clearType issues in IE6 and IE7. Can be short-circuited by setting
      * cancel to false. Addresses tickets #2457, #3230, #4779 and others.
     */
     var _fadeIn = $.fn.fadeIn;
-    
+
     $.fn.fadeIn = function(easing, callback, cancel) {
         return _fadeIn.call(this, easing, function(){
             if(jQuery.browser.msie && !cancel) {
                 $(this).get(0).style.removeAttribute('filter');
             }
-            if($.isFunction(callback)) callback();                
+            if($.isFunction(callback)) callback();
         });
-    }       
+    }
 
 
     var _fadeOut = $.fn.fadeOut;
-    
+
     $.fn.fadeOut = function(easing, callback, cancel) {
         return _fadeOut.call(this, easing, function() {
             if(jQuery.browser.msie && !cancel) {
                 $(this).get(0).style.removeAttribute('filter');
             }
-            if($.isFunction(callback)) callback();                
+            if($.isFunction(callback)) callback();
         });
-    }       
+    }
 
 })(jQuery);
